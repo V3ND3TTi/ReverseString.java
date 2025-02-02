2e7f88a45af812f75d28d439096d06a03fe0e066
Devon Vendetti <v3nd3tti@icloud.com>
2025-02-02 06:32:46 -0800

    Java Reverse String

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        String orig = "Hello, World!";
+        String orig2 = "StringBuilder Version!";
+        String reversed = reverseString(orig);
+        String reversed2 = reverseStringSB(orig2);
+        System.out.println(reversed);
+        System.out.println(reversed2);
+    }
+
+//    public static String reverseString(String str) {
+//        char[] chars = str.toCharArray();
+//        int left = 0, right = chars.length - 1;
+//        while (left < right) {
+//            char temp = chars[left];
+//            chars[left] = chars[right];
+//            chars[right] = temp;
+//            left++;
+//            right--;
+//        }
+//        return new String(chars);
+//    }
+
+    public static String reverseString(String str) {
+        char[] chars = str.toCharArray();
+        String result = "";
+        for (int i = chars.length - 1; i >= 0; i--) {
+            result += chars[i];
+        }
+        return result;
+    }
+
+    public static String reverseStringSB(String str) {
+        char[] chars = str.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            result.append(chars[i]);
+        }
+        return result.toString();
+    }
+}
